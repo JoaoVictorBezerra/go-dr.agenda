dc454406c7ddaf2473d906224f6d3b40b1e8c69f
João Victor <dev.joaovictor@hotmail.com>
2025-05-28 13:28:40 -0300

    feat: add database connection

---
+++ database/conn.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     string = "localhost"
+	port     string = "5432"
+	user     string = "user"
+	password string = "password"
+	dbName   string = "dr_agenda"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to " + dbName)
+
+	return db, nil
+}
